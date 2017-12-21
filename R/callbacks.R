b6c8dc43b0a0ca892f66022e89a339a080549412
peterhurford <peter@peterhurford.com>
2017-12-21 00:36:43 -0600

    prettify json

--- R/callbacks.R
+++ R/callbacks.R
@@ -3,5 +3,5 @@
 #' @param r. The raw response from the httr call.
 #' @export
 write_json_callback <- function(r) {
-  httr::content(r, as = "text", encoding = "UTF-8")
+  jsonlite::prettify(httr::content(r, as = "text", encoding = "UTF-8"))
 }
