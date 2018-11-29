2199cebc59202b2f326fcac483eabc7c01e73fe1
Peter Hurford <peter@peterhurford.com>
2018-11-28 20:33:26 -0800

    Snapshotting

--- R/record.R
+++ R/record.R
@@ -10,11 +10,11 @@ record <- function(expr, method = httr::GET, file = "vcr.txt", snapshot = FALSE,
                    callback = function(r) capture.output(dput(r))) {
   original_method <- method
   verb <- strsplit(deparse(substitute(method)), "::")[[1]][[2]]
-  stubbed_method <- function(..., vcr_callback = callback, vcr_file = file) {
+  stubbed_method <- function(..., vcr_callback = callback, vcr_file = file, is_snapshot = snapshot) {
     unlink(vcr_file)
     response <- original_method(...)
-    if (isTRUE(snapshot)) {
-      saveRDS(vcr_callback(response), paste0(vcr_file, ".RDS"))
+    if (isTRUE(is_snapshot)) {
+      saveRDS(vcr_callback(response), vcr_file)
     } else {
       cat(vcr_callback(response), file = vcr_file)
       cat("\n", file = vcr_file, append = TRUE)
